728475d1211c3a523780e13d531b5bf879bc320f
Cesar Laserna <cesar_laserna09@hotmail.com>
2018-07-15 18:51:58 -0500

    Like and dislike system for reply

--- app/Http/Controllers/LikesController.php
+++ app/Http/Controllers/LikesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Likes;
 use Illuminate\Http\Request;
+use App\Model\Reply;
 
 class LikesController extends Controller
 {
@@ -82,4 +83,31 @@ class LikesController extends Controller
     {
         //
     }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function likeIt(Reply $reply)
+    {
+        //
+        $reply->like()->create([
+            // 'user_id' => auth()->id();
+            'user_id' => '1'
+        ]);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function unLikeIt(Reply $reply)
+    {
+        //
+        // $reply->like()->where(['user_id',auth()->id()])->first()->delete();
+        $reply->like()->where('user_id','1')->first()->delete();
+    }
+
 }
